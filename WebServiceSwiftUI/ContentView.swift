138b58c9af9b0395f9b9747a35c254ddeb4f91f9
mjoselli <mejoselli@gmail.com>
2022-05-11 15:35:46 -0300

    Initial Commit

---
+++ WebServiceSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebServiceSwiftUI
+//
+//  Created by Mark Joselli on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebServiceSwiftUI/WebServiceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebServiceSwiftUIApp.swift
+//  WebServiceSwiftUI
+//
+//  Created by Mark Joselli on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WebServiceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
